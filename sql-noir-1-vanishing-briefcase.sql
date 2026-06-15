ce19e77735ace7f60b11d1e9ba00d3f4b7a06b78
Seth Patterson <dailydoseofseth@gmail.com>
2026-06-15 14:40:04 -0600

    Add SQL queries for Vanishing Briefcase investigation
    Added SQL queries to investigate the Vanishing Briefcase case, including retrieving crime scene details, identifying suspects, and verifying them through interviews.

--- sql-noir-1-vanishing-briefcase.sql
+++ sql-noir-1-vanishing-briefcase.sql
@@ -2,3 +2,38 @@
   -- 1. The SQL queries you ran
   -- 2. Any notes or insights as SQL comments
   -- 3. Your final conclusion: who did it?
+
+-- Case #001: The Vanishing Briefcase
+-- Set in the gritty 1980s, a valuable briefcase has disappeared from the Blue Note Lounge. A witness reported that a man in a trench coat was seen fleeing the scene. Investigate the crime scene, review the list of suspects, and examine interview transcripts to reveal the culprit.
+
+-- Objectives
+-- 1.
+-- Retrieve the correct crime scene details to gather the key clue.
+-- 2.
+-- Identify the suspect whose profile matches the witness description.
+-- 3.
+-- Verify the suspect using their interview transcript.
+
+---------------------
+SELECT * FROM crime_scene
+WHERE type = 'theft';
+
+SELECT *
+FROM crime_scene
+WHERE type = 'theft'
+AND location = 'Blue Note Lounge';
+
+SELECT * FROM suspects;
+
+SELECT *
+FROM suspects
+WHERE attire = 'trench coat'
+AND scar = 'left cheek';
+
+SELECT *
+FROM interviews
+WHERE suspect_id IN (3, 183);
+
+SELECT *
+FROM suspects
+WHERE id = 183;
