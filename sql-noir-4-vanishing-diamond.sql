f2a495ac3d6faef350ee46624ab88c360a644e3b
Seth Patterson <dailydoseofseth@gmail.com>
2026-06-17 09:57:37 -0600

    Record SQL queries and insights for diamond theft case
    Documented SQL detective process for the missing diamond case, including queries and insights.

--- sql-noir-4-vanishing-diamond.sql
+++ sql-noir-4-vanishing-diamond.sql
@@ -2,3 +2,42 @@
   -- 1. The SQL queries you ran
   -- 2. Any notes or insights as SQL comments
   -- 3. Your final conclusion: who did it?
+
+-- At Miami’s prestigious Fontainebleau Hotel charity gala, the famous “Heart of Atlantis” diamond necklace suddenly disappeared from its display.
+
+-- Objectives
+-- 1.
+-- Find who stole the diamond.
+
+SELECT * 
+FROM final_interviews
+WHERE confession NOT LIKE '%kill%';
+
+-------this was a good move as it showed my right at the top of the list 105 saying they did it!-----
+
+SELECT * FROM guest WHERE id = 105;
+
+---Mike Manning---
+
+----but just to look around more...----
+
+SELECT * 
+FROM witness_statements w
+JOIN guest g ON w.guest_id = g.id;
+
+SELECT * 
+FROM witness_statements w
+JOIN guest g ON w.guest_id = g.id
+WHERE g.invitation_code = 'VIP-R';
+
+----a load of witness CLUEs-----meh...
+----aha! more proof----
+
+SELECT g.name, g.occupation, a.note
+FROM attire_registry a
+JOIN guest g ON a.guest_id = g.id
+WHERE a.note LIKE '%navy%' AND a.note LIKE '%white tie%';
+
+----Mike Manning shows up in QUERY---
+---Boom----
+
